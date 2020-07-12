920b57da4650bca525a21410f61ce61965adc93e
Iacchus Mercurius <kassivs.@gmail.com>
2020-07-12 20:05:04 -0300

    ok

---
+++ test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+from utils import colorize
+
+a = colorize('oi')
+print(a)
--- utils/fzf.py
+++ utils/fzf.py
@@ -1,12 +1,14 @@
 from subprocess import Popen, PIPE
 
-def wrapper(input_data, charset='utf-8', decode=True, strip=True):
+def wrapper(input_data, fzf_options=None, charset='utf-8', decode=True,
+            strip=True):
     """Wrapper for fzf.
 
     Pipes `string` to fzf and returns response or None in case of error
 
     Args:
         string (byte, str or list): Data to be sent to fzf stdin.
+        fzf_options (dict): Options ins the format `{'--option': 'value'}`
         charset (str): Charset used for converting `bytes` <-> `str`.
         decode (bool): Wether to decode fzf `bytes` response to `str`.
         strip (bool): Wether to strip newlines from fzf response.
@@ -23,7 +25,13 @@ def wrapper(input_data, charset='utf-8', decode=True, strip=True):
     else:
         return None
 
-    fzf_proc = Popen('fzf', shell=True, stdin=PIPE, stdout=PIPE)
+    if fzf_options:
+        fzf_args = " ".join([f"{option} {value}"
+                             for option, value in fzf_options])
+    else:
+        fzf_args = ""
+
+    fzf_proc = Popen(['fzf', fzf_args], shell=True, stdin=PIPE, stdout=PIPE)
     output, err = fzf_proc.communicate(input=str_to_pipe_in)
 
     if decode:  # decode bytes to charset str
