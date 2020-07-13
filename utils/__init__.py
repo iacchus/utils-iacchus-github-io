2bd167d5a1abf85b89e368bf353226edd01ae9ea
Iacchus Mercurius <kassivs.@gmail.com>
2020-07-13 19:18:37 -0300

    ok

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,27 @@ import re
 from . import fzf
 from . import colorize
 
+# http://code.activestate.com/recipes/134892/
+class Getch:
+    """Gets a single character from standard input.  Does not echo to the
+screen."""
+
+    def __init__(self):
+
+        import tty
+        import sys
+
+    def __call__(self):
+
+        import sys
+        import tty
+        import termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
